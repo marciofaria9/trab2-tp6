ef75bcc4b3cfb11cc5deae7dbe1fcebfdafe270b
Marcio Faria <m.faria.jr@icloud.com>
2022-11-08 19:53:04 -0300

    adding automation

--- init.sql
+++ init.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS partidas (
     selecao_visitante varchar(30) not null,
     gol_mandante Integer not null,
     gol_visitante Integer not null
-
+ 
 );
 
 insert into partidas (Id, data_partida, selecao_mandante,selecao_visitante,gol_mandante,gol_visitante)
